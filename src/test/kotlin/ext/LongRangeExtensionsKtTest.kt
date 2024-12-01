16d71fc3311db37dbe7e5d841434c5c6302487e6
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2024-12-01 16:09:15 +0100

    Fix accidental rename (I have no idea how this happened)

--- src/test/kotlin/ext/LongRangeExtensionsKtTest.kt
+++ src/test/kotlin/ext/LongRangeExtensionsKtTest.kt
@@ -37,43 +37,43 @@ class LongRangeExtensionsKtTest {
 
     @Test
     fun `difference - returns null if range is contained within`() {
-        val difference = (1L..10L).distance(2L..6L)
+        val difference = (1L..10L).difference(2L..6L)
         assertThat(difference).isEmpty()
     }
 
     @Test
     fun `difference - returns given range if it is fully outside of it`() {
-        val difference = (1L..10L).distance(15L..20L)
+        val difference = (1L..10L).difference(15L..20L)
         assertThat(difference).isEqualTo(listOf(15L..20L))
     }
 
     @Test
     fun `difference - returns part that does not overlap at the lower end`() {
-        val difference = (5L..10L).distance(2L..6L)
+        val difference = (5L..10L).difference(2L..6L)
         assertThat(difference).isEqualTo(listOf(2L..4L))
     }
 
     @Test
     fun `difference - returns part that does not overlap at the upper end`() {
-        val difference = (10L..14L).distance(5L..14L)
+        val difference = (10L..14L).difference(5L..14L)
         assertThat(difference).isEqualTo(listOf(5L..9L))
     }
 
     @Test
     fun `difference - returns parts that do not overlap at the lower and upper end if given range fully contains range`() {
-        val difference = (5L..10L).distance(1L..15L)
+        val difference = (5L..10L).difference(1L..15L)
         assertThat(difference).isEqualTo(listOf(1L..4L, 11L..15L))
     }
 
     @Test
     fun `difference - returns parts that do not overlap at the lower and upper end if given range fully contains range upper exact`() {
-        val difference = (5L..10L).distance(1L..10L)
+        val difference = (5L..10L).difference(1L..10L)
         assertThat(difference).isEqualTo(listOf(1L..4L))
     }
 
     @Test
     fun `difference - returns parts that do not overlap at the lower and upper end if given range fully contains range lower exact`() {
-        val difference = (5L..10L).distance(5L..15L)
+        val difference = (5L..10L).difference(5L..15L)
         assertThat(difference).isEqualTo(listOf(11L..15L))
     }
 
