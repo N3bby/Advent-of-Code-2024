aefc4d1669a68caa2c9aa6caf8e72998be6fa373
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2024-12-01 15:26:08 +0100

    Add script to generate templates for a specific day

---
+++ generate-day.sh
@@ -0,0 +1,25 @@
+DAY=$1
+
+touch input/day"${DAY}".txt
+
+mkdir src/main/kotlin/day"${DAY}"
+echo "package day${DAY}" > src/main/kotlin/day"${DAY}"/Day"${DAY}".kt
+
+mkdir src/test/kotlin/day"${DAY}"
+echo "package day${DAY}
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day${DAY}Test {
+
+    @Test
+    fun \`part 1 - example input\`() {
+        val input = \"\"\"
+
+        \"\"\".trimIndent()
+
+    }
+
+}
+" > src/test/kotlin/day"${DAY}"/Day"${DAY}"Test.kt
