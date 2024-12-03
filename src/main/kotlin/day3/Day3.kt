e3e87bc0fddee5cc4b4a211dc28f5270a03416ea
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2024-12-03 17:15:51 +0100

    Day 3 part 2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -1,23 +1,55 @@
 package day3
 
-data class MultiplicationInstruction(private val instruction: String) {
-    override fun toString(): String = instruction
+sealed class Instruction {
+    data object Do : Instruction()
 
-    private val operands by lazy {
-        Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
-            .find(instruction)!!.groupValues
-            .let { Pair(it[1].toInt(), it[2].toInt()) }
+    data object Dont : Instruction()
+
+    data class Multiplication(private val instruction: String): Instruction() {
+        override fun toString(): String = instruction
+
+        private val operands by lazy {
+            Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
+                .find(instruction)!!.groupValues
+                .let { Pair(it[1].toInt(), it[2].toInt()) }
+        }
+
+        fun execute(): Int {
+            val (a, b) = operands
+            return a * b
+        }
     }
 
-    fun execute(): Int {
-        val (a, b) = operands
-        return a * b
+    companion object {
+        fun buildInstruction(instruction: String): Instruction = when {
+            instruction.startsWith("do(") -> Do
+            instruction.startsWith("don't(") -> Dont
+            instruction.startsWith("mul(") -> Multiplication(instruction)
+            else -> throw IllegalArgumentException("Unsupported instruction: $instruction")
+        }
     }
 }
 
-fun findValidMultiplicationInstructions(memory: String): List<MultiplicationInstruction> {
+
+fun findValidMultiplicationInstructions(memory: String): List<Instruction.Multiplication> {
     val matches = Regex("""mul\(\d{1,3},\d{1,3}\)""").findAll(memory).toList()
-    return matches.map { MultiplicationInstruction(it.value) }
+    return matches.map { Instruction.Multiplication(it.value) }
 }
 
-fun List<MultiplicationInstruction>.sum(): Int = sumOf { it.execute() }
+fun findValidInstructions(memory: String): List<Instruction> {
+    val matches = Regex("""don't\(\)|do\(\)|mul\(\d{1,3},\d{1,3}\)""").findAll(memory).toList()
+    return matches.map { Instruction.buildInstruction(it.value) }
+}
+
+fun List<Instruction.Multiplication>.sum(): Int = sumOf { it.execute() }
+
+fun List<Instruction>.collapseConditionals(): List<Instruction.Multiplication> {
+    var enabled = true
+    val activeInstructions = mutableListOf<Instruction.Multiplication>()
+    forEach { instruction -> when (instruction) {
+        is Instruction.Do -> enabled = true
+        is Instruction.Dont -> enabled = false
+        is Instruction.Multiplication -> if (enabled) activeInstructions.add(instruction)
+    } }
+    return activeInstructions
+}
--- src/test/kotlin/day3/Day3Test.kt
+++ src/test/kotlin/day3/Day3Test.kt
@@ -15,10 +15,10 @@ class Day3KtTest {
         val instructions = findValidMultiplicationInstructions(input)
 
         assertThat(instructions).containsExactly(
-            MultiplicationInstruction("mul(2,4)"),
-            MultiplicationInstruction("mul(5,5)"),
-            MultiplicationInstruction("mul(11,8)"),
-            MultiplicationInstruction("mul(8,5)")
+            Instruction.Multiplication("mul(2,4)"),
+            Instruction.Multiplication("mul(5,5)"),
+            Instruction.Multiplication("mul(11,8)"),
+            Instruction.Multiplication("mul(8,5)")
         )
 
         assertThat(instructions.sum()).isEqualTo(161)
@@ -31,6 +31,35 @@ class Day3KtTest {
         val instructions = findValidMultiplicationInstructions(input)
 
         assertThat(instructions.sum()).isEqualTo(0)
+    }
+
+    @Test
+    fun `part 2 - example input`() {
+        val input = """
+            xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        """.trimIndent()
+
+        val instructions = findValidInstructions(input)
+
+        assertThat(instructions).containsExactly(
+            Instruction.Multiplication("mul(2,4)"),
+            Instruction.Dont,
+            Instruction.Multiplication("mul(5,5)"),
+            Instruction.Multiplication("mul(11,8)"),
+            Instruction.Do,
+            Instruction.Multiplication("mul(8,5)")
+        )
+
+        assertThat(instructions.collapseConditionals().sum()).isEqualTo(48)
+    }
+
+    @Test
+    fun `part 2 - puzzle input`() {
+        val input = readInput(3)
+
+        val instructions = findValidInstructions(input)
+
+        assertThat(instructions.collapseConditionals().sum()).isEqualTo(0)
     }
 }
 
