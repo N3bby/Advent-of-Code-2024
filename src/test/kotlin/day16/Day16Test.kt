1a3befba91729a9a587140e716fecaeafdad5cf6
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2024-12-17 03:06:36 +0100

    Day 16 part 2

--- src/test/kotlin/day16/Day16Test.kt
+++ src/test/kotlin/day16/Day16Test.kt
@@ -3,7 +3,6 @@ package day16
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import util.Grid
-import util.Position
 import util.readInput
 
 class Day16KtTest {
@@ -140,7 +139,7 @@ class Day16KtTest {
 
         val toCsv = Grid.generate(maze.bounds) {
             when {
-                maze.nodes.contains(it) -> maze.distances[it]!!.toString() + if(positionsPartOfShortestPath.contains(it)) " O" else ""
+                maze.nodes.contains(it) -> maze.distances[it]!!.toString() + if (positionsPartOfShortestPath.contains(it)) " O" else ""
                 else -> '#'
             }
         }.toCsv()
@@ -177,7 +176,7 @@ class Day16KtTest {
 
         val toCsv = Grid.generate(maze.bounds) {
             when {
-                maze.nodes.contains(it) -> maze.distances[it]!!.toString() + if(positionsPartOfShortestPath.contains(it)) " O" else ""
+                maze.nodes.contains(it) -> maze.distances[it]!!.toString() + if (positionsPartOfShortestPath.contains(it)) " O" else ""
                 else -> '#'
             }
         }.toCsv()
@@ -194,16 +193,7 @@ class Day16KtTest {
         val paths = maze.getShortestPaths()
         val positionsPartOfShortestPath = paths.flatMap { it }.toSet()
 
-        val toCsv = Grid.generate(maze.bounds) {
-            when {
-                maze.nodes.contains(it) -> maze.distances[it]!!.toString() + if(positionsPartOfShortestPath.contains(it)) " O" else ""
-                else -> '#'
-            }
-        }.toCsv()
-        println(toCsv)
-
-        // Too high: 512
-        assertThat(positionsPartOfShortestPath.size).isEqualTo(0)
+        assertThat(positionsPartOfShortestPath.size).isEqualTo(471)
     }
 }
 
