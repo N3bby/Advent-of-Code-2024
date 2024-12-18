8078ee2535494062235296d5c5eaed1a9a362ed7
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2024-12-18 23:01:38 +0100

    Day 17 part 2

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -19,14 +19,23 @@ sealed class ProgramElement {
             else -> throw IllegalStateException("Unknown operand: $value")
         }
 
+        fun printComboValue(computer: Computer): String = when (value) {
+            in 0L..3L -> "$value (literal)"
+            4L -> "${computer.registers.a} (A)"
+            5L -> "${computer.registers.b} (B)"
+            6L -> "${computer.registers.c} (C)"
+            7L -> throw IllegalStateException("Reserved operand: 7")
+            else -> throw IllegalStateException("Unknown operand: $value")
+        }
+
         fun getLiteralValue(): Long = value
 
     }
 
-    data class Instruction(private val type: InstructionType) : ProgramElement() {
+    data class Instruction(val type: InstructionType) : ProgramElement() {
 
-        fun execute(operand: Operand, computer: Computer): Computer {
-            val updatedComputer = type.execute(operand, computer)
+        fun execute(operand: Operand, computer: Computer, print: Boolean): Computer {
+            val updatedComputer = type.execute(operand, computer, print)
             return if (type == InstructionType.JNZ && updatedComputer.instructionPointer != computer.instructionPointer) {
                 updatedComputer
             } else {
@@ -38,33 +47,37 @@ sealed class ProgramElement {
 
 }
 
-enum class InstructionType(val opcode: Int, val execute: (Operand, Computer) -> Computer) {
-    ADV(0, { operand, computer ->
+enum class InstructionType(val opcode: Int, val execute: (Operand, Computer, Boolean) -> Computer) {
+    ADV(0, { operand, computer, print ->
         val operandValue = operand.getComboValue(computer)
         val aValue = computer.registers.a
+        if (print) println("ADV: $aValue (A) / 2 ^ ${operand.printComboValue(computer)} --> A")
 
         val result = aValue / 2.0.pow(operandValue.toDouble())
 
         computer.updateRegisterA(result.toLong())
     }),
-    BXL(1, { operand, computer ->
+    BXL(1, { operand, computer, print ->
         val operandValue = operand.getLiteralValue()
         val bValue = computer.registers.b
+        if (print) println("BXL: $bValue (B) xor $operandValue (literal) --> B")
 
         val result = bValue.xor(operandValue)
 
         computer.updateRegisterB(result)
     }),
-    BST(2, { operand, computer ->
+    BST(2, { operand, computer, print ->
         val operandValue = operand.getComboValue(computer)
+        if (print) println("BST: ${operand.printComboValue(computer)} % 8 --> B")
 
         val result = operandValue % 8
 
         computer.updateRegisterB(result)
     }),
-    JNZ(3, { operand, computer ->
+    JNZ(3, { operand, computer, print ->
         val operandValue = operand.getLiteralValue()
         val aValue = computer.registers.a
+        if (print) println("JNZ: if ($aValue (A)) { jumpTo($operandValue (literal)) }")
 
         if(aValue != 0L) {
             computer.copy(instructionPointer = operandValue.toInt())
@@ -72,32 +85,36 @@ enum class InstructionType(val opcode: Int, val execute: (Operand, Computer) -> 
             computer
         }
     }),
-    BXC(4, { operand, computer ->
+    BXC(4, { operand, computer, print ->
         val bValue = computer.registers.b
         val cValue = computer.registers.c
+        if (print) println("BXC: $bValue (B) xor $cValue (C) --> B")
 
         val result = bValue.xor(cValue)
 
         computer.updateRegisterB(result)
     }),
-    OUT(5, { operand, computer ->
+    OUT(5, { operand, computer, print ->
         val operandValue = operand.getComboValue(computer)
+        if (print) println("OUT: ${operand.printComboValue(computer)} % 8 --> OUTPUT")
 
         val result = operandValue % 8
 
         computer.writeOutput(result)
     }),
-    BDV(6, { operand, computer ->
+    BDV(6, { operand, computer, print ->
         val operandValue = operand.getComboValue(computer)
         val aValue = computer.registers.a
+        if (print) println("BDV: $aValue (A) / 2 ^ ${operand.printComboValue(computer)} --> B")
 
         val result = aValue / 2.0.pow(operandValue.toDouble())
 
         computer.updateRegisterB(result.toLong())
     }),
-    CDV(7, { operand, computer ->
+    CDV(7, { operand, computer, print ->
         val operandValue = operand.getComboValue(computer)
         val aValue = computer.registers.a
+        if (print) println("CDV: $aValue (A) / 2 ^ ${operand.printComboValue(computer)} --> C")
 
         val result = aValue / 2.0.pow(operandValue.toDouble())
 
@@ -110,7 +127,11 @@ enum class InstructionType(val opcode: Int, val execute: (Operand, Computer) -> 
     }
 }
 
-data class Registers(val a: Long, val b: Long, val c: Long)
+data class Registers(val a: Long, val b: Long, val c: Long) {
+    override fun toString(): String {
+        return "A=$a, B=$b, C=$c"
+    }
+}
 
 data class Computer(
     private val program: List<ProgramElement>,
@@ -119,13 +140,16 @@ data class Computer(
     private val output: List<Long> = emptyList(),
 ) {
 
-    fun runUntilHalt(): Computer {
+    fun runUntilHalt(print: Boolean = false): Computer {
         var computer = this
         while (computer.instructionPointer < program.size) {
             val instruction = program[computer.instructionPointer] as Instruction
             val operand = program[computer.instructionPointer + 1] as Operand
 
-            computer = instruction.execute(operand, computer)
+            computer = instruction.execute(operand, computer, false)
+            if(print) {
+                println(computer.registers)
+            }
         }
         return computer
     }
@@ -137,6 +161,14 @@ data class Computer(
 
     fun writeOutput(value: Long) = copy(output = output + value)
     fun printOutput(): String = output.joinToString(",")
+    fun rawOutput(): List<Int> = output.map { it.toInt() }
+
+    fun getRawProgram(): List<Int> = program.map { element ->
+        when(element) {
+            is Operand -> element.getLiteralValue().toInt()
+            is Instruction -> element.type.opcode
+        }
+    }
 
     companion object
 
--- src/test/kotlin/day17/Day17Test.kt
+++ src/test/kotlin/day17/Day17Test.kt
@@ -26,41 +26,75 @@ class Day17KtTest {
     @Test
     fun `instructions should produce the correct results`() {
         // Divides A by 2^combo, writes to A
-        assertThat(ADV.execute(Operand(2), computer(8, 0, 0))).isEqualTo(computer(2, 0, 0))
+        assertThat(ADV.execute(Operand(2), computer(8, 0, 0), false)).isEqualTo(computer(2, 0, 0))
 
         // XORs B and literal operand, writes to B.
         // e.g. 110 and 010 XOR to 100
-        assertThat(BXL.execute(Operand(6), computer(0, 2, 0))).isEqualTo(computer(0, 4, 0))
+        assertThat(BXL.execute(Operand(6), computer(0, 2, 0), false)).isEqualTo(computer(0, 4, 0))
 
         // Calculates combo % 8, writes to B
-        assertThat(BST.execute(Operand(5), computer(0, 12, 0))).isEqualTo(computer(0, 4, 0))
+        assertThat(BST.execute(Operand(5), computer(0, 12, 0), false)).isEqualTo(computer(0, 4, 0))
 
         // Does nothing when A is 0
         // Jumps to literal when A is not 0
-        assertThat(JNZ.execute(Operand(2), computer(0, 0, 0))).isEqualTo(computer(0, 0, 0))
-        assertThat(JNZ.execute(Operand(17), computer(1, 0, 0))).isEqualTo(computer(1, 0, 0).copy(instructionPointer = 17))
+        assertThat(JNZ.execute(Operand(2), computer(0, 0, 0), false)).isEqualTo(computer(0, 0, 0))
+        assertThat(JNZ.execute(Operand(17), computer(1, 0, 0), false)).isEqualTo(
+            computer(1, 0, 0).copy(
+                instructionPointer = 17
+            )
+        )
 
         // XORs B and C, writes to B
-        assertThat(BXC.execute(Operand(2), computer(0, 6, 2))).isEqualTo(computer(0, 4, 2))
+        assertThat(BXC.execute(Operand(2), computer(0, 6, 2), false)).isEqualTo(computer(0, 4, 2))
 
         // Calculates combo % 8, writes to output
-        assertThat(OUT.execute(Operand(3), computer(0, 0, 0))).isEqualTo(computer(0, 0, 0).copy(output = listOf(3)))
+        assertThat(OUT.execute(Operand(3), computer(0, 0, 0), false)).isEqualTo(
+            computer(
+                0,
+                0,
+                0
+            ).copy(output = listOf(3))
+        )
 
         // Divides A by 2^combo, writes to B
-        assertThat(BDV.execute(Operand(2), computer(8, 0, 0))).isEqualTo(computer(8, 2, 0))
+        assertThat(BDV.execute(Operand(2), computer(8, 0, 0), false)).isEqualTo(computer(8, 2, 0))
 
         // Divides A by 2^combo, writes to C
-        assertThat(CDV.execute(Operand(2), computer(8, 0, 0))).isEqualTo(computer(8, 0, 2))
+        assertThat(CDV.execute(Operand(2), computer(8, 0, 0), false)).isEqualTo(computer(8, 0, 2))
     }
 
     @Test
     fun `part 1 - puzzle input`() {
         val input = readInput(17)
 
-        val computer = parseComputer(input).runUntilHalt()
+        val computer = parseComputer(input).runUntilHalt(print = true)
 
         assertThat(computer.printOutput()).isEqualTo("1,6,7,4,3,0,5,0,6")
 
+    }
+
+    @Test
+    fun `part 2 - puzzle input`() {
+        val input = readInput(17)
+        val computer = parseComputer(input)
+
+        val program = computer.getRawProgram()
+        val validValuesForAPerIteration: List<MutableList<Long>> = (0 until program.size).map { mutableListOf() }
+        for (index in program.indices) {
+            val resultToProduce = program.takeLast(index + 1)
+
+            val validValuesForPreviousIteration = if (index == 0) listOf(0L) else validValuesForAPerIteration[index - 1]
+            validValuesForPreviousIteration.forEach { previousValidValue ->
+                val valuesToTest = (0L until 8L).map { value -> previousValidValue * 8L + value }
+                val validValues = valuesToTest.filter { value ->
+                    val haltedComputer = computer.updateRegisterA(value).runUntilHalt()
+                    haltedComputer.rawOutput() == resultToProduce
+                }
+                validValuesForAPerIteration[index].addAll(validValues)
+            }
+        }
+
+        assertThat((validValuesForAPerIteration.last()).min()).isEqualTo(216148338630253)
     }
 }
 
